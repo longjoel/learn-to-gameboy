19fc91e678f1f77029d5bbeb3929a76bf03b4a89
Joel Longanecker <Joel.Longanecker@gmail.com>
2021-08-07 19:40:36 -0500

    fix packagelock

--- day-2/scripts/build.js
+++ day-2/scripts/build.js
@@ -4,6 +4,11 @@ const fs = require('fs');
 const cp = require('child_process');
 const path = require('path');
 
+const { promisify } = require('util');
+const { resolve } = require('path');
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+
 const root = path.join(__dirname, '..');
 const objPath = path.join(root, 'obj');
 const binPath = path.join(root, 'bin');
@@ -15,6 +20,15 @@ const LCC = path.join(GBDK_ROOT, 'bin','lcc.exe');
 
 const WINE = process.platform.toLowerCase() == 'linux' ? 'wine':'';
 const PS = process.platform.toLowerCase() == 'linux' ? '/':'\\';
+
+async function getFiles(dir) {
+    const subdirs = await readdir(dir);
+    const files = await Promise.all(subdirs.map(async (subdir) => {
+      const res = resolve(dir, subdir);
+      return (await stat(res)).isDirectory() ? getFiles(res) : res;
+    }));
+    return files.reduce((a, f) => a.concat(f), []);
+  }
 
 // make sure to include the include path
 let ccFlags = [...pkg.ccFlags, '-I'+path.join(GBDK_ROOT,'include')];
@@ -54,27 +68,34 @@ const link = (objFiles, name) => {
         ].join(' '));
 }
 
-if (fs.existsSync(objPath)) {
-    rmrf.sync(objPath);
-}
 
-if (fs.existsSync(binPath)) {
-    rmrf.sync(binPath);
-}
 
-mkdirp.sync(objPath,{recursive:true});
-mkdirp.sync(binPath,{recursive:true});
-
-const findSrcFiles = () => {
-    const files = query(`find ./src -type f \\( -name "*.c" \\)`);
+const findSrcFiles = async () => {
+    const files = (await getFiles( path.join(root,'src') )).filter(x=> x.endsWith('.c')); //readDirp() //query(`find ./src -type f \\( -name "*.c" \\)`);
     return files.map(x => path.resolve(x));
 }
 
-findSrcFiles().forEach(x => {
-    console.log('building: ' + x);
-    compile(x);
-});
+(async ()=>{
 
-link(findSrcFiles()
+    if (fs.existsSync(objPath)) {
+        rmrf.sync(objPath);
+    }
+    
+    if (fs.existsSync(binPath)) {
+        rmrf.sync(binPath);
+    }
+    
+mkdirp.sync(objPath,{recursive:true});
+mkdirp.sync(binPath,{recursive:true});
+
+    (await findSrcFiles()).forEach(x => {
+        console.log('building: ' + x);
+        compile(x);
+    });
+
+    link((await findSrcFiles())
     .map(x => srcToObj(x)),
     path.join(root, 'bin', pkg.name));
+})();
+
+
